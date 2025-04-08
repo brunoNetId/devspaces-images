881244c95859927a9c2b3a7465d36019de268cee
Samantha Dawley <sdawley@redhat.com>
2025-04-08 10:21:56 -0700

    CRW-8592 Update sync-che-olm script to add ppc64le and s390x labels to existing labels instead of attempting to create a second labels section. (#751)

--- devspaces-operator-bundle/build/scripts/sync-che-olm.sh
+++ devspaces-operator-bundle/build/scripts/sync-che-olm.sh
@@ -225,7 +225,7 @@ for CSVFILE in ${TARGETDIR}/manifests/devspaces.csv.yaml; do
 		-e '/operatorframework.io\/cluster-monitoring:/d' \
 		-e 's|operatorframework.io/suggested-namespace: .+|operatorframework.io/suggested-namespace: openshift-operators|' \
 		-e '/operatorframework.io\/suggested-namespace/a \ \ \ \ operatorframework.io/cluster-monitoring: "true"\n'"$subscriptions" \
-		-e '/annotations\:/i \ \ labels:\n    operatorframework.io/arch.amd64\: supported\n    operatorframework.io/arch.ppc64le\: supported\n    operatorframework.io/arch.s390x\: supported' \
+		-e '/annotations\:/i \ \   operatorframework.io/arch.ppc64le\: supported\n    operatorframework.io/arch.s390x\: supported' \
 		-e 's|devworkspace-devspaces-operator|devworkspace-che-operator|' \
 		-e 's|"namespace": ".+"|"namespace": "openshift-devspaces"|' \
 		-i "${CSVFILE}"
